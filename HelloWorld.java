46aa7c071f7728259e179ccb06ac72a78e003ec8
anshuman4code <51124927+anshuman4code@users.noreply.github.com>
2019-06-04 19:28:49 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, I am here in the terminal window.
+   public static void main(String[] ar) {
+      // Prints "Hello, I am here in the terminal window.neww
       System.out.println("Hello all, Welcome to Winning Team");
    }
 }
